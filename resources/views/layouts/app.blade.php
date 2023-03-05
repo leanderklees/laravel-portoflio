8c357e3dfc37da1e3a4292985c6ffb9bf0a1b146
Leander Klees <leanderklees89@gmail.com>
2023-03-05 13:18:25 +0100

    added fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        <script src="https://kit.fontawesome.com/ca5173a060.js" crossorigin="anonymous"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
@@ -35,17 +36,26 @@
         <div class="alertbox">
             @if(session()->has('success'))
                 <div class="alert" role="alert">
+                    <i class="fa fa-circle-check mr-2 text-green-600"></i>
                     {{ session()->get('success') }}
                 </div>
             @endif
             @if(session()->has('status'))
                 <div class="alert" role="alert">
+                    <i class="fa fa-circle-info  mr-2 text-blue-400"></i>
                     {{ session()->get('status') }}
                 </div>
             @endif
             @if(session()->has('error'))
                 <div class="alert" role="alert">
+                    <i class="fa fa-bomb mr-2 text-red-600"></i>
                     {{ session()->get('error') }}
+                </div>
+            @endif
+            @if(session()->has('warning'))
+                <div class="alert" role="alert">
+                    <i class="fa fa-triangle-exclamation text-amber-400"></i>
+                    {{ session()->get('status') }}
                 </div>
             @endif
         </div>
